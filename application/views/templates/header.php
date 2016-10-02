9589e6abc00088a1f8742104fb75bf4a526a7090
Fhaical <fhaicalfhc@gmail.com>
2016-10-03 02:49:46 +0700

    add navbar

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -1,7 +1,28 @@
 <html>
-        <head>
-                <title>EVFund</title>
-        </head>
-        <body>
+    <head>
+        <title>EVFund</title>
+        
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Arvo">
+        <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+        <link rel="stylesheet" href="styles.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+        <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+    </head>
+    
+    <body>
+        <div class="container-fluid" style="background: url('source/header.jpg');color:#FFF;height:200px;">
+            <h1 class="header_text">EVFund</h1>
+        </div>
 
-                <h1><?php echo $title; ?></h1>
+        <nav class="navbar navbar-inverse" data-spy="affix" data-offset-top="197">
+            <ul class="nav navbar-nav">
+                <li><a href=#>home</a></li>
+                <li><a href=#>about</a></li>
+                <li><a href=#>help</a></li>
+                <li><a href=#>contact</a></li>
+                <li><a href=#>sign up</a></li>
+                <li><a href=#>log in</a></li>
+            </ul>
+        </nav>
