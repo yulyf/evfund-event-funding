7f0fc1a3b79d1516525a2cf526ba4a874fa890cd
Aldifiati Arfiani <tetrafriends@gmail.com>
2016-10-09 11:13:04 +0700

    base url nya diganti

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost:8080/evfund-event-funding-kppl-d-e/';
+$config['base_url'] = 'http://localhost:8080/KPPL/evfund-event-funding-kppl-d-e/';
 
 /*
 |--------------------------------------------------------------------------
