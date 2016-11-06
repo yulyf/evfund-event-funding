d12f292b2ed31ac41e396d3b9c95e7b152d6947b
Muhammad Atsil Husni Rafi <mas_rafi@yahoo.com>
2016-11-06 14:25:57 +0700

    ganti code , tambah tampilan , bisa ke database

--- application/views/upload/v_upload.php
+++ application/views/upload/v_upload.php
@@ -1,21 +1,49 @@
+<!DOCTYPE html>
 <html>
 <head>
-	<title>EVFUND WEBSITE</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>File Upload Form</title>
+    <!-- load bootstrap css file -->
+    <link href="<?php echo base_url("assets/css/bootstrap.css"); ?>" rel="stylesheet" type="text/css" />
 </head>
 <body>
-	<center><h1>UPLOAD Proposal </h1></center>
-	<?php echo $error;?>
+<div class="container">
+    <div class="row">
+        <div class="col-md-6 col-md-offset-3 well">
+        <legend>Ev-Fund File Upload</legend>
+        <?php echo form_open_multipart('upload/upload');?>
 
-	<?php echo form_open_multipart('upload/aksi_upload');?>
+        <fieldset>
+            <div class="form-group">
+                <div class="row">
+                    <div class="col-md-12">
+                        <label for="filename" class="control-label">Select File to Upload</label>
+                    </div>
+                </div>
+            </div>
 
-	<input type="file" name="berkas" />
+            <div class="form-group">
+                <div class="row">
+                    <div class="col-md-12">
+                        <input type="file" name="file1" size="20" />
+                        <span class="text-danger"><?php if (isset($error)) { echo $error; } ?></span>
+                    </div>
+                </div>
+            </div>
 
-	<br /><br />
-
-	<input type="submit" value="upload" />
-
-</form>
-	
+            <div class="form-group">
+                <div class="row">
+                    <div class="col-md-12">
+                        <input type="submit" value="Upload File" class="btn btn-primary"/>
+                    </div>
+                </div>
+            </div>
+        </fieldset>
+        
+        <?php echo form_close(); ?>
+        <?php if (isset($success_msg)) { echo $success_msg; } ?>
+        </div>
+    </div>
+</div>
 </body>
-
 </html>
