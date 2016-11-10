967df0053a06cccf1bc2c26da6768058d8f62b62
Muhammad Atsil Husni Rafi <mas_rafi@yahoo.com>
2016-11-10 20:59:58 +0700

    memperindah tampilan

--- application/views/lihat/v_lihat.php
+++ application/views/lihat/v_lihat.php
@@ -13,6 +13,60 @@ $result = mysqli_query($koneksi, $sql);
     <meta content="width=device-width, initial-scale=1.0" name="viewport" >
     <link rel="stylesheet" href="assets/css/bootstrap.css" type="text/css" />
 </head>
+head>
+	<meta charset="utf-8">
+	<meta name="viewport"    content="width=device-width, initial-scale=1.0">
+	<meta name="description" content="">
+	<meta name="author"      content="Sergey Pozhilov (GetTemplate.com)">
+	
+	<title>Ev-Fund</title>
+
+	<link rel="shortcut icon" href="assets/images/gt_favicon.png">
+	
+	<link rel="stylesheet" media="screen" href="http://fonts.googleapis.com/css?family=Open+Sans:300,400,700">
+	<link rel="stylesheet" href="assets/css/bootstrap.min.css">
+	<link rel="stylesheet" href="assets/css/font-awesome.min.css">
+
+	<!-- Custom styles for our template -->
+	<link rel="stylesheet" href="assets/css/bootstrap-theme.css" media="screen" >
+	<link rel="stylesheet" href="assets/css/main.css">
+
+	<!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+	<!--[if lt IE 9]>
+	<script src="assets/js/html5shiv.js"></script>
+	<script src="assets/js/respond.min.js"></script>
+	<![endif]-->
+</head>
+
+<body class="home">
+	<!-- Fixed navbar -->
+	<div class="navbar navbar-inverse navbar-fixed-top headroom" >
+		<div class="container">
+			<div class="navbar-header">
+				<!-- Button for smallest screens -->
+				<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse"><span class="icon-bar"></span> <span class="icon-bar"></span> <span class="icon-bar"></span> </button>
+				<a class="navbar-brand" href="welcome"></a>
+			</div>
+			<div class="navbar-collapse collapse">
+				<ul class="nav navbar-nav pull-right">
+					<li class="active"><a href="welcome">Home</a></li>
+					<li><a href="lihat">Lihat</a></li>
+					<li><a href="upload">Upload Proposal</a></li>
+					<li><a class="btn" href="logout">Log Out</a></li>
+				</ul>
+			</div><!--/.nav-collapse -->
+		</div>
+	</div> 
+	<!-- /.navbar -->
+	
+		<div class="container text-center">
+		<br> <br>
+		<h2 class="thin">EvFund Dokumen</h2>
+		<p class="text-muted">
+			Anda ingin melihat sebuah event ? <br> 
+			silahktan temukan mereka disini.
+		</p>
+	</div>
 <body>
 <br/>
 <div class="row">
